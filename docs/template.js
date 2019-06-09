2e4450339ec180bf0ca02068b2873da8560737b3
Y-Reku <r48484@st.takushoku-u.ac.jp>
2019-06-09 18:07:27 +0900

    add javascript

---
+++ docs/template.js
@@ -0,0 +1,15 @@
+function menu(){
+
+    var html = "";
+;
+    html += '<nav>';
+	html +=		'<ul>';
+	html +=		'<li><a href="index.html">Top</a></li>';
+	html +=		'<li><a href=”#”>About</a></li>';
+	html +=		'<li><a href=“#”>Member</a></li>';
+	html +=		'<li><a href=”#”>Contact</a></li>';
+	html +=		'</ul>';
+	html +=	'</nav>';
+   
+    document.write(html);
+}
