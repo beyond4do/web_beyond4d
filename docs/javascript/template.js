62745f572c7a9b0dbe8e259cc5d98519f8b2f712
Qooto <51712492+Qooto@users.noreply.github.com>
2019-06-22 23:27:38 +0900

    スマホ対応解除

--- docs/javascript/template.js
+++ docs/javascript/template.js
@@ -10,7 +10,7 @@ function head(){
   var html = '';
       html += '<meta charset="UTF-8">';
       html += '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
-      html += '<meta name="viewport" content="width=device-width,initial-scale=1">';
+//      html += '<meta name="viewport" content="width=device-width,initial-scale=1">';
       html += '<link rel="stylesheet" type="text/css" href="css/style.css">';
       html += '<link rel="shortcut icon" href="img/LogoW.ico"/>';
       html += '<script src="https://unpkg.com/ionicons@4.1.2/dist/ionicons.js"></script>';
